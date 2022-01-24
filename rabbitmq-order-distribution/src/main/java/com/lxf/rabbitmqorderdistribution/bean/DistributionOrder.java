7d9f476e666c5c66fa7a0ced0c6f185a3a27b9e7
LLLLLL <913577706@qq.com>
2022-01-24 17:52:06 +0800

    pojo

---
+++ rabbitmq-order-distribution/src/main/java/com/lxf/rabbitmqorderdistribution/bean/DistributionOrder.java
@@ -0,0 +1,57 @@
+package com.lxf.rabbitmqorderdistribution.bean;
+
+/**
+ * @author LiXiaoFeng
+ * @date 2022年01月24日 17:49
+ */
+public class DistributionOrder {
+    private int orderId;
+
+    private String msg;
+
+    private String userId;
+
+    private String delFlag;
+
+    private String createTime;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ rabbitmq-order-service/src/main/java/com/lxf/rabbitmqorderservice/bean/ServiceOrder.java
@@ -0,0 +1,78 @@
+package com.lxf.rabbitmqorderservice.bean;
+
+/**
+ * @author LiXiaoFeng
+ * @date 2022年01月24日 17:51
+ */
+public class ServiceOrder {
+
+    private int serviceId;
+
+    private int orderId;
+
+    private String msg;
+
+    private String status;
+
+    private String riderId;
+
+    private String delFlag;
+
+    private String createTime;
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRiderId() {
+        return riderId;
+    }
+
+    public void setRiderId(String riderId) {
+        this.riderId = riderId;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
